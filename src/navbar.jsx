9a740366a472cbcb2ecfddca3cecd5aff77ba6bc
BaronLiu1993 <117589118+BaronLiu1993@users.noreply.github.com>
2024-03-24 23:40:02 -0400

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -8,8 +8,8 @@ function navbar() {
       <div className = 'bg-[#101628] flex-col flex items-end'>
         <ol className = 'flex p-2 justify-end '>
           <Link to = "home" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>01. Home</Link>
-          <Link to = "portfolio" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>02. About Me!</Link>
-          <Link to = "about" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>03. Extracurriculars</Link>
+          <Link to = "about" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>02. About Me!</Link>
+          <Link to = "portfolio" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>03. Extracurriculars</Link>
           <Link to = "projects" spy = {true} smooth = {true} offset = {50} duration = {500} href = '#' className = 'm-4 font-semibold hover:font-extrabold text-white'>04. Projects!</Link>
         </ol>
       </div>
